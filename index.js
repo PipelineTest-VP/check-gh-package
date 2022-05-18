72c59ca43f70e36812544f51fe979476705c3114
Vishnu Prabhakar <vishnuprabhakar@Vishnus-MacBook-Pro.local>
2022-05-18 12:28:40 -0400

    Intial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const { Octokit } = require("@octokit/rest");
+
+let octokit;
+
+async function main() {
+    try {
+        const orgName = core.getInput('gthub-org-name');
+        const gthubPackageType = core.getInput('gthub-package-type');
+        const gthubPackageName = core.getInput('gthub-package-name');
+        const gthubPackageVersion = core.getInput('gthub-package-version');
+        const gthubToken = core.getInput('gthub-token');
+
+        octokit = new Octokit({ auth: gthubToken });
+
+        const response = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+            package_type: gthubPackageType,
+            package_name: gthubPackageName,
+            org: orgName
+        });
+
+        console.log("response data: ", response.data);
+        
+        let isPackageFound = false;
+        const packageVersions = response.data.length;
+        for(let i = 0; i < packageVersions; i++) {
+            if(response.data[i].name === gthubPackageVersion.toString()) {
+                isPackageFound = true;
+                break;
+            }
+        }
+        console.log("IS_PACKAGE_FOUND: ", isPackageFound);
+
+        core.exportVariable("IS_PACKAGE_FOUND", isPackageFound);
+        
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error.message);
+    }
+}
+
+main();
